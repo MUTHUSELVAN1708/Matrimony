9acba13d194d63e9c0719b647fcfcf71e3b2b14f
MUTHUSELVAN1708 <128883246+MUTHUSELVAN1708@users.noreply.github.com>
2024-11-06 16:00:01 +0530

    Revert "test change 3 (#2)"
    This reverts commit 3b3827a4647d6e12961faf3f8d235e3baa756115.

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
     return      ProviderScope(
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter',
+        title: 'Flutter Demo',
         // home:  RegisterUserPhotoUploadScreen(),
         home: InitialScreen(),
       ),
